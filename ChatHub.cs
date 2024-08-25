8a56181ef78d22c98cdce1c9adc4c61958f31332
Olomu Davis Oghenemiren <olomu.davis@ymail.com>
2024-08-25 01:20:01 +0100

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChatApp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ChatHub : Hub
+    {
+        // Method to send a message to all connected clients
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
